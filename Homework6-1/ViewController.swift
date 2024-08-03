9ef54f676f5f5cad1490f7d8238a085e3d081ef0
TL-Hung <tsunlong0816@gmail.com>
2024-08-03 22:40:29 +0800

    Initial Commit

---
+++ Homework6-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework6-1
+//
+//  Created by TL on 2024/8/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
